7af86e3dd11d1f13978350b66962a5cc7896af12
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 11:38:29 -0700

    Q12 ts/js of 45 assignment
    Q12 ts/js of 45 assignment

---
+++ q12.js
@@ -0,0 +1,10 @@
+//Q12.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
+//print a message to them. The text of each message should be the same,
+// but each message should be personalized with the person’s name.
+var Names = ["Izna", "Adv haya", "rekha", "ravisha", "sonia"];
+var massage = "how are you dear?";
+console.log(Names[0] + " " + massage);
+console.log(Names[1] + " " + massage);
+console.log(Names[2] + " " + massage);
+console.log(Names[3] + " " + massage);
+console.log(Names[4] + " " + massage);
